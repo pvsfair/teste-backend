3b205e3d7f1c0468fd486258c4014df6b34a14a0
Paulo Victor Alvares <pvsfair@gmail.com>
2019-10-05 00:00:49 -0300

    Configurando MongoDB

--- config/database.php
+++ config/database.php
@@ -35,6 +35,15 @@ return [
 
     'connections' => [
 
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'dsn' => env('DB_DSN'),
+            'database' => env('DB_DATABASE'),
+            'options'  => [
+                'database' => 'admin' // sets the authentication database required by mongo 3
+            ]
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
